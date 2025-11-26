1f86eb8db7ac57bd775bd0d5f5db4b5c931e15c8
ismail baoud <ibaoud46@egmail.com>
2025-11-26 09:04:50 +0100

    Add ClientRepository and UserRepository interfaces for Client and User entities

---
+++ src/main/java/com/ismail/smartShop/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.ismail.smartShop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ismail.smartShop.model.Client;
+
+public interface ClientRepository extends JpaRepository<Client,Long>{
+    
+}
