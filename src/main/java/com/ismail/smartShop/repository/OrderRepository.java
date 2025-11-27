0981f9863dca3114f556d55b1d080cc2fd37811a
ismail baoud <ibaoud46@egmail.com>
2025-11-27 22:18:20 +0100

    Enhance repositories: add methods for stock management in ProductRepository and implement findAllByClientId in OrderRepository

--- src/main/java/com/ismail/smartShop/repository/OrderRepository.java
+++ src/main/java/com/ismail/smartShop/repository/OrderRepository.java
@@ -1,9 +1,14 @@
 package com.ismail.smartShop.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ismail.smartShop.dto.orderItem.Request.OrderItemRequest;
 import com.ismail.smartShop.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Collection<OrderItemRequest> findAllByClientId(Long id);
 
 }
