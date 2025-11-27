2c9c82e55a92e6f5d71d7c86a780e01d0fd5d056
ismail baoud <ibaoud46@egmail.com>
2025-11-27 22:15:21 +0100

    Enhance OrderRequest class with validation annotations and constructors

--- src/main/java/com/ismail/smartShop/dto/order/request/OrderRequest.java
+++ src/main/java/com/ismail/smartShop/dto/order/request/OrderRequest.java
@@ -2,13 +2,25 @@ package com.ismail.smartShop.dto.order.request;
 
 import java.util.List;
 
+
 import com.ismail.smartShop.dto.orderItem.Request.OrderItemRequest;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderRequest {
     private Long client_id;
     private String promo;
+    @Nullable
+    private Double tva;
+    @NotEmpty(message = "The order must contain at least 1 item")
+    @Size(min = 1, message = "The order must contain at least 1 item")
     private List<OrderItemRequest> items;
 }
