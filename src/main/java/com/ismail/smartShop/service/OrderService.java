890e6fb155b8bb0a831367a14f0fd407601ae054
ismail baoud <ibaoud46@egmail.com>
2025-11-28 23:30:40 +0100

    Add PaymentService interface for payment processing operations

--- src/main/java/com/ismail/smartShop/service/OrderService.java
+++ src/main/java/com/ismail/smartShop/service/OrderService.java
@@ -1,8 +1,18 @@
 package com.ismail.smartShop.service;
 
+import java.util.List;
+
 import com.ismail.smartShop.dto.order.request.OrderRequest;
 import com.ismail.smartShop.dto.order.response.OrderResponse;
 
 public interface OrderService {
     public OrderResponse createOrder(OrderRequest orderRequest);
+
+    public List<OrderResponse> getAllOrders();
+
+    public List<OrderResponse> getAllOrdersOfClient(Long id);
+
+    public OrderResponse cancelOrder(Long id);
+
+    public OrderResponse confirmOrder(Long id);
 }
