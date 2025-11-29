5d4e897debbd56a4bb5fff88e97c6ee14b2c3699
ismail baoud <ibaoud46@egmail.com>
2025-11-29 22:26:41 +0100

    Implement payment creation logic with status handling and order confirmation updates

--- src/main/java/com/ismail/smartShop/service/implementation/PaymentServiceImpl.java
+++ src/main/java/com/ismail/smartShop/service/implementation/PaymentServiceImpl.java
@@ -12,6 +12,8 @@ import com.ismail.smartShop.mapper.PaymentMapper;
 import com.ismail.smartShop.model.Client;
 import com.ismail.smartShop.model.Order;
 import com.ismail.smartShop.model.Payment;
+import com.ismail.smartShop.model.enums.OrderStatus;
+import com.ismail.smartShop.model.enums.PaymentMethod;
 import com.ismail.smartShop.model.enums.PaymentStatus;
 import com.ismail.smartShop.repository.ClientRepository;
 import com.ismail.smartShop.repository.OrderRepository;
@@ -28,14 +30,33 @@ public class PaymentServiceImpl implements PaymentService {
     private final PaymentMapper paymentMapper;
     private final OrderRepository orderRepository;
     private final ClientRepository clientRepository;
+    private final ClientServiceImpl clientService;
 
     @Override
     public PaymentResponse createPayment(Long id, PaymentRequest paymentReq) {
         Order order = orderRepository.findById(id).orElseThrow(()-> new RuntimeException());
-
+        Client client = order.getClient();
         Payment payment = paymentMapper.toEntity(paymentReq);
         payment.setOrder(order);
-        return paymentMapper.toDto(paymentRepository.save(payment));
+        payment.setBanque(paymentReq.getBanque());
+        payment.setDatePaiement(LocalDateTime.now());
+        payment.setStatus(PaymentStatus.PENDING);
+        payment.setMethod(PaymentMethod.valueOf(paymentReq.getMethod()));
+        if (order.getMontant_restant() < payment.getAmount()) {
+            throw new RuntimeException("this amount is haist than the rest amoun , plase enter just the rest amount : "+order.getMontant_restant());
+        }
+        
+        PaymentResponse pr = paymentMapper.toDto(paymentRepository.save(payment));
+        order.setMontant_restant(order.getMontant_restant() - payment.getAmount());
+        if(order.getMontant_restant() == 0) {
+            order.setStatus(OrderStatus.CONFIRMED);
+            client.setTotalCommandes(client.getTotalCommandes() + 1);
+            client.setTotalDepense(client.getTotalDepense() + payment.getAmount());
+            client = clientService.changeFidelite(client);
+            clientRepository.save(client);
+        }
+        orderRepository.save(order);
+        return pr;
     }
 
     @Override
