1d2448b813dc36ca9739e5932c2f3307da52e1c8
ismail baoud <ibaoud46@egmail.com>
2025-11-30 21:37:37 +0100

    Add method-level access control annotations to PaymentController, ProductController, and PromoController

--- src/main/java/com/ismail/smartShop/controller/ProductController.java
+++ src/main/java/com/ismail/smartShop/controller/ProductController.java
@@ -3,6 +3,9 @@ package com.ismail.smartShop.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ismail.smartShop.annotation.RequireAdmin;
+import com.ismail.smartShop.annotation.RequireAuth;
+import com.ismail.smartShop.annotation.RequireClient;
 import com.ismail.smartShop.dto.product.request.ProductRequest;
 import com.ismail.smartShop.dto.product.response.ProductResponse;
 import com.ismail.smartShop.service.ProductService;
@@ -31,26 +34,36 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
+    @RequireAdmin
+    @RequireAuth
     public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest req) {
         return ResponseEntity.ok().body(productService.createProduct(req));
     }
     
     @GetMapping
+    @RequireAuth
+    @RequireAdmin
     public ResponseEntity<List<ProductResponse>> getAllProducts() {
         return ResponseEntity.ok().body(productService.getAllProducts());
     }
     
     @GetMapping("/{id}")
+    @RequireAuth
+    @RequireAdmin
     public ResponseEntity<ProductResponse> getProduct(@PathVariable("id") Long id) {
         return ResponseEntity.ok().body(productService.getProductById(id));
     }
     
     @PutMapping("/{id}")
+    @RequireAuth
+    @RequireAdmin
     public ResponseEntity<ProductResponse> updateProduct(@Valid @PathVariable Long id, @RequestBody ProductRequest req) {
         return ResponseEntity.ok().body(productService.updateProduct(id, req));
     }
 
     @DeleteMapping("/{id}")
+    @RequireAuth
+    @RequireAdmin
     public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
         return ResponseEntity.ok().body("product deleted successfully");
